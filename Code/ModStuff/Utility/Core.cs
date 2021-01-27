779a33ca6001ba5bddfb9ab50a58e2d785e0e716
Chris is Awesome <chrisstarkofficial@gmail.com>
2021-01-27 00:04:53 -0800

    Add GetEntComp()

---
+++ Code/ModStuff/Utility/Core.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace ModStuff.Utility
+{
+	public static class Core
+	{
+		public static T GetEntComp<T>(string entName) where T : Component
+		{
+			GameObject go = GameObject.Find(entName);
+
+			if (go != null)
+			{
+				// Check if component is on the GameObject
+				T foundComp = go.GetComponent<T>();
+				if (foundComp != null) { return foundComp; }
+
+				// Check if component is on any of its  children
+				foreach (Transform trans in go.transform)
+				{
+					foundComp = trans.GetComponent<T>();
+					if (foundComp != null) { return foundComp; }
+				}
+			}
+			else
+			{
+				// If GameObject not found
+				DebugManager.LogDebugMessage("Entity with name '" + entName + "' was not found. Returning null.", LogType.Error);
+				return null;
+			}
+
+			// If component not found
+			DebugManager.LogDebugMessage("Component of type '" + typeof(T).ToString() + "' was not found on Entity named '" + entName + "'. Returning null.", LogType.Warning);
+			return null;
+		}
+	}
+}
