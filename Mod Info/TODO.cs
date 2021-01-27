39347137e83ebd87fcc9480f9ccdcc725b14dfe5
Chris is Awesome <chrisstarkofficial@gmail.com>
2021-01-27 00:14:00 -0800

    1/26/21

--- Mod Info/TODO.cs
+++ Mod Info/TODO.cs
@@ -1,1 +1,6 @@
-// Literally everything...
+/* Important
+ * Debug Menu test command
+ * Add existing save file support to SaveManager
+ * Add custom file support to SaveManager
+ * Make custom json file for storing mod hotkeys (eg. key for opening debug menu)
+ */
