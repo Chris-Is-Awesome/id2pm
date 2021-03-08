e5bfc80f6d2a6b5332aeb355cbbd7beb349d8dd0
Chris is Awesome <chrisstarkofficial@gmail.com>
2021-03-07 17:30:00 -0800

    Add console logger

--- Code/ModStuff/Utility/DebugManager.cs
+++ Code/ModStuff/Utility/DebugManager.cs
@@ -5,6 +5,14 @@ namespace ModStuff.Utility
 {
 	public static class DebugManager
 	{
+		public enum MessageType
+		{
+			Info,
+			Success,
+			Warn,
+			Error
+		}
+
 		public static void LogDebugMessage(string message, LogType logType = LogType.Log, bool includeStackTrace = true, bool isHeader = false, bool addWhiteSpace = true)
 		{
 			string output = FormatDebugMessage(message, logType, isHeader, addWhiteSpace);
@@ -25,6 +33,38 @@ namespace ModStuff.Utility
 			}
 
 			Debug.Log(output);
+		}
+
+		public static string LogDebugMessageToConsole(string message,  MessageType type, bool doFormat = true)
+		{
+			string output = string.Empty;
+			string infoColor = "<color=#141414>";
+			string successColor = "<color=#078716>";
+			string warningColor = "<color=#c95e00>";
+			string errorColor = "<color=#db1414>";
+
+			switch (type)
+			{
+				case MessageType.Success:
+					if (doFormat) output = successColor;
+					break;
+				case MessageType.Warn:
+					if (doFormat) output = warningColor;
+					output += "WARNING: ";
+					break;
+				case MessageType.Error:
+					if (doFormat) output = errorColor;
+					output += "ERROR: ";
+					break;
+				default:
+					if (doFormat) output = infoColor;
+					break;
+			}
+
+			output += message;
+			if (doFormat) output += "</color>";
+
+			return output;
 		}
 
 		private static string FormatDebugMessage(string message, LogType logType, bool isHeader, bool addWhiteSpace)
