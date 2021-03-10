5452c4d9b5cb3fb21da4fb97b01efa69009622c6
Chris is Awesome <chrisstarkofficial@gmail.com>
2021-03-09 18:17:23 -0800

    Update to speed command

---
+++ Code/Updated Files/RigidBodyController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using UnityEngine;
+
+[AddComponentMenu("Ittle 2/Physics/Rigidbody controller")]
+public class RigidBodyController : MonoBehaviour
+{
+	Rigidbody realBody;
+
+	BC_Body betterbody;
+
+	Vector3 accumVel;
+
+	Vector3 previousPosition;
+
+	public float velocityMultiplier; // Added
+
+	void Awake()
+	{
+		velocityMultiplier = 1; // Set velocity to default (for some reason setting it on initialization doesn't do this???)
+
+		this.realBody = base.GetComponent<Rigidbody>();
+		if (this.realBody != null)
+		{
+			this.realBody.freezeRotation = true;
+			this.realBody.useGravity = false;
+		}
+		else
+		{
+			this.betterbody = base.GetComponent<BC_Body>();
+			if (this.betterbody != null)
+			{
+				this.betterbody.UseGravity = false;
+			}
+		}
+	}
+
+	void OnDisable()
+	{
+		this.accumVel = Vector3.zero;
+	}
+
+	public void SetVelocity(Vector3 vel)
+	{
+		// Multiply velocity
+		if (velocityMultiplier != 1) this.accumVel += vel * velocityMultiplier;
+		else this.accumVel += vel;
+	}
+
+	public void MovePosition(Vector3 P)
+	{
+		if (this.realBody != null)
+		{
+			this.realBody.MovePosition(P);
+		}
+		else
+		{
+			this.betterbody.transform.position = P;
+		}
+		this.accumVel = Vector3.zero;
+	}
+
+	public void LateUpdate()
+	{
+		if (this.realBody != null)
+		{
+			this.realBody.velocity = this.accumVel;
+		}
+		else
+		{
+			this.betterbody.Velocity = this.accumVel;
+		}
+		this.accumVel = Vector3.zero;
+	}
+}
