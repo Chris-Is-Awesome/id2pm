5cda8a364115a106e187512da06effec90ce8747
Chris is Awesome <chrisstarkofficial@gmail.com>
2021-03-09 19:27:07 -0800

    haha easter egg :)

--- Code/ModStuff/Cheats/HelpCommand.cs
+++ Code/ModStuff/Cheats/HelpCommand.cs
@@ -6,6 +6,11 @@ namespace ModStuff.Cheats
 {
 	public class HelpCommand : Singleton<HelpCommand>
 	{
+		public static string GetHelp()
+		{
+			return "You really need help <i>here</i>?! At this point, you really should consult Tippsie...";
+		}
+
 		public string RunCommand(string[] args)
 		{
 			string className = "ModStuff.Cheats." + char.ToUpper(args[0][0]) + args[0].Substring(1) + "Command";
