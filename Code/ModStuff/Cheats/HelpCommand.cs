4eaf578d854a672d20063fed1a4ba4211154bfc5
Chris is Awesome <chrisstarkofficial@gmail.com>
2021-03-09 19:23:30 -0800

    Cleanup

--- Code/ModStuff/Cheats/HelpCommand.cs
+++ Code/ModStuff/Cheats/HelpCommand.cs
@@ -8,10 +8,8 @@ namespace ModStuff.Cheats
 	{
 		public string RunCommand(string[] args)
 		{
-			// Convert arg0 to class name (generic?) and return GetHelp() ????????????
-			// Bad way: switch
-			string className = char.ToUpper(args[0][0]) + args[0].Substring(1) + "Command";
-			MethodInfo command = Type.GetType("ModStuff.Cheats." + className).GetMethod("GetHelp");
+			string className = "ModStuff.Cheats." + char.ToUpper(args[0][0]) + args[0].Substring(1) + "Command";
+			MethodInfo command = Type.GetType(className).GetMethod("GetHelp");
 			return DebugManager.LogToConsole(command.Invoke(null, null).ToString());
 		}
 	}
