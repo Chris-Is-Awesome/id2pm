62d42c56246686425d791b8716ace30af80109df
Chris is Awesome <chrisstarkofficial@gmail.com>
2021-03-12 16:55:35 -0800

    Add PerformanceTester

---
+++ Code/ModStuff/PerformanceTester.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace ModStuff
+{
+	public static class PerformanceTester
+	{
+		public static Stopwatch StartStopwatch()
+		{
+			Stopwatch stopwatch = new Stopwatch();
+			stopwatch.Start();
+			return stopwatch;
+		}
+
+		public static float StopStopwatch(Stopwatch stopwatch)
+		{
+			stopwatch.Stop();
+			return stopwatch.ElapsedMilliseconds;
+		}
+	}
+}
