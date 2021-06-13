0c63f4a9b8e4ea7deab4bf5d71b5a45d2ed5cb7d
Chris is Awesome <chrisstarkofficial@gmail.com>
2021-06-13 12:59:13 -0700

    Make field public

---
+++ Code/Updated Files/DropTable.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+
+[PersistentScriptableObject]
+public class DropTable : ScriptableObject
+{
+	[SerializeField]
+	public ItemBase[] _items; // Made public
+
+	[SerializeField]
+	bool _randomize;
+
+	public bool IsEmpty
+	{
+		get
+		{
+			if (this._items == null || this._items.Length == 0)
+			{
+				return true;
+			}
+			for (int i = 0; i < this._items.Length; i++)
+			{
+				if (this._items[i] != null)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+
+	public int NumItems
+	{
+		get
+		{
+			return this._items.Length;
+		}
+	}
+
+	public bool Randomize
+	{
+		get
+		{
+			return this._randomize;
+		}
+	}
+
+	public ItemBase GetNextItem(DropTableContext context)
+	{
+		int num = context.NextPositionForTable(this);
+		if (num >= 0 && num < this._items.Length)
+		{
+			return this._items[num];
+		}
+		return null;
+	}
+}
---
+++ Code/Updated Files/DropTableContext.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class DropTableContext
+{
+	Dictionary<string, int> savedStates = new Dictionary<string, int>();
+
+	public Dictionary<DropTable, DropTableContext.TableState> tables = new Dictionary<DropTable, DropTableContext.TableState>(); // Made public
+
+	public int NextPositionForTable(DropTable table)
+	{
+		DropTableContext.TableState tableState;
+		if (!this.tables.TryGetValue(table, out tableState))
+		{
+			int pos;
+			if (!this.savedStates.TryGetValue(table.name, out pos))
+			{
+				pos = 0;
+			}
+			tableState = new DropTableContext.TableState(table, pos);
+			this.tables.Add(table, tableState);
+		}
+		return tableState.AdvancePosition();
+	}
+
+	public void SaveState(IDataSaver saver)
+	{
+		foreach (KeyValuePair<DropTable, DropTableContext.TableState> keyValuePair in this.tables)
+		{
+			saver.SaveInt(keyValuePair.Key.name, keyValuePair.Value.Position);
+		}
+	}
+
+	public void LoadState(IDataSaver saver)
+	{
+		this.tables = new Dictionary<DropTable, DropTableContext.TableState>();
+		this.savedStates = new Dictionary<string, int>();
+		string[] allDataKeys = saver.GetAllDataKeys();
+		for (int i = 0; i < allDataKeys.Length; i++)
+		{
+			this.savedStates[allDataKeys[i]] = saver.LoadInt(allDataKeys[i]);
+		}
+	}
+
+	public class TableState // Made public
+	{
+		DropTable table;
+
+		int pos;
+
+		List<int> randomIndices;
+
+		public TableState(DropTable table, int pos = 0)
+		{
+			this.table = table;
+			this.pos = pos;
+			if (table.Randomize)
+			{
+				this.randomIndices = DropTableContext.TableState.Randomize(table.NumItems);
+			}
+		}
+
+		static List<int> Randomize(int num)
+		{
+			List<int> list = new List<int>(num);
+			while (--num >= 0)
+			{
+				list.Add(num);
+			}
+			ListUtility.Shuffle<int>(list);
+			return list;
+		}
+
+		public int Position
+		{
+			get
+			{
+				return this.pos;
+			}
+		}
+
+		public int AdvancePosition()
+		{
+			int num = this.pos++;
+			if (this.randomIndices != null)
+			{
+				num = this.randomIndices[num];
+			}
+			if (this.pos >= this.table.NumItems)
+			{
+				this.pos = 0;
+				if (this.table.Randomize)
+				{
+					this.randomIndices = DropTableContext.TableState.Randomize(this.table.NumItems);
+				}
+			}
+			return num;
+		}
+	}
+}
